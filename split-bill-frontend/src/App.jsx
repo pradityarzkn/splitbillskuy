d1949f58d2b407231b3bb3b6d567ef87a578a3cd
pradityarzkn <pradityarizkin@gmail.com>
2026-04-22 12:12:12 +0700

    fix api url

--- split-bill-frontend/src/App.jsx
+++ split-bill-frontend/src/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
   // ---- Submit ----
   const handleSubmit = async () => {
     try {
-      const res = await axios.post("https://splitbillskuy-api.onrender.com", {
+      const res = await axios.post("https://splitbillskuy-api.onrender.com/calculate", {
         paidBy,
         tax: Number(tax || 0),
         service: Number(service || 0),
